d0fa3006e37272f38fe24e843db749554e3d58d8
Alisson <alissonvpb18@gmail.com>
2026-09-04 15:57:23 -0300

    Criando funções para verificar as jogadas disponíveis e simular uma partida entre o bot e a IA

--- test_minimax.py
+++ test_minimax.py
@@ -2,3 +2,34 @@ import random
 
 from minimax import obter_melhor_jogada
 from cli import verificar_vencedor
+
+def jogadas_disponiveis(tabuleiro):
+    return[i for i in range(9) if tabuleiro[i] == '']
+
+def simular_partida(bot_primeiro=true):
+    tabuleiro = [' '] * 9
+
+    if bot_primeiro:
+        simbolo_x = 'X'
+        simbolo_aleatorio = 'O'
+        turno_bot = True
+    else:
+        simbolo_bot = 'O'
+        simbolo_aleatorio = 'X'
+        turno_bot = False
+
+    while True:
+        if turno_bot:
+            posicao = obter_melhor_jogada(tabuleiro, simbolo_bot)
+            tabuleiro[posicao] = simbolo_bot
+        else:
+            posicao = random.choice(jogadas_disponiveis(tabuleiro))
+            tabuleiro[posicao] = simbolo_aleatorio
+
+        resultado = verificar_vencedor(tabuleiro)
+
+        if resultado is not None:
+            return resultado
+        
+        turno_bot = not turno_bot
+
